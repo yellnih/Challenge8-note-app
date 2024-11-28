24b708a86bcc0f5f3cfa6f3f86dbba9f70edac88
yellnih <znlszk98@gmail.com>
2024-11-28 16:27:51 +0900

    firebase_login

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
+import '../home/home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
